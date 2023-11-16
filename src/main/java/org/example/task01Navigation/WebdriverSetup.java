38d45036896dc4df58c73aac4eb959dd27146019
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2023-11-16 19:13:29 +0100

    add WebdriverSetup.java

---
+++ src/main/java/org/example/task01Navigation/WebdriverSetup.java
@@ -0,0 +1,27 @@
+package org.example.task01Navigation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebdriverSetup {
+
+    private static WebDriver driver;
+    private static final String PATH_TO_WEBDRIVER = "/usr/bin/chromedriver";
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", PATH_TO_WEBDRIVER);
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
