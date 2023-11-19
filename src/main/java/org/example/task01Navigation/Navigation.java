8ef93148f64c857375ea1f7f99361b8a04fa5707
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2023-11-19 19:47:36 +0100

    delete unused code from <Navigation.java>

--- src/main/java/org/example/task01Navigation/Navigation.java
+++ src/main/java/org/example/task01Navigation/Navigation.java
@@ -1,12 +1,8 @@
 package org.example.task01Navigation;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class Navigation {
-    WebDriver driver;
 
     public static void navigateToSimpleFormDemo(WebDriver driver) {
         InputFormsPage inputFormsPage = new InputFormsPage(driver);
