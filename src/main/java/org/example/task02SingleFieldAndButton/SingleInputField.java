007f8e53e4744b1f94987828a323d156eaf69e54
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2023-11-19 21:39:40 +0100

    add <SingleInputField.java>

---
+++ src/main/java/org/example/task02SingleFieldAndButton/SingleInputField.java
@@ -0,0 +1,39 @@
+package org.example.task02SingleFieldAndButton;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SingleInputField {
+    WebDriver driver;
+    private final static String MESSAGE = "Hello, I am here!";
+    @FindBy(id = "user-message") WebElement singleInputField;
+
+    @FindBy(xpath = "//*[@id=\"get-input\"]/button") WebElement messageDisplayer;
+
+    public SingleInputField(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickSingleInputField() {
+        singleInputField.click();
+    }
+
+
+    public void enterTextToSingleInputField(String message) {
+        singleInputField.sendKeys(message);
+    }
+
+    public void showMessage() {
+        messageDisplayer.click();
+        messageDisplayer.getText();
+    }
+
+    public void runSingleInput(String message) {
+        clickSingleInputField();
+        enterTextToSingleInputField(message);
+        showMessage();
+    }
+}
